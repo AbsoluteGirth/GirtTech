6aef1111b7cfb54399a46f2123e50e250d8b11c1
AbsoluteGirth <adrury37@gmail.com>
2022-07-09 09:10:50 +0100

    dsadssa

--- OpenGateAdv.lua
+++ OpenGateAdv.lua
@@ -68,7 +68,8 @@ local currentpos = 0
 if oddeven == even then
     if axis == "z" then
         while currentpos <= dist do
-            exec("fill "..x1.." "..y1.." "..(center1-currentpos).." "..x2.." "..y2.." "..(center2-currentpos).." minecraft:air")
+            exec("fill "..x1.." "..y1.." "..(center1-currentpos).." "..x2.." "..y2.." "..(center1-currentpos).." minecraft:air")
+            exec("fill "..x1.." "..y1.." "..(center2+currentpos).." "..x2.." "..y2.." "..(center2+currentpos).." minecraft:air")
             sleep(tonumber(speed))
             currentpos = currentpos + 1
         end
