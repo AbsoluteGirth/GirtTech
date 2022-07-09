c580f435915b77a934fbecb19329552acf4b7829
AbsoluteGirth <adrury37@gmail.com>
2022-07-09 08:31:13 +0100

    Check center

--- OpenGateAdv.lua
+++ OpenGateAdv.lua
@@ -6,14 +6,17 @@ local args = ...
 -- Door setup --
 
 local speed = "0.5"
-local block = ""
-local x1 = ""
-local z1 = ""
-local x2 = ""
-local z2 = ""
-local y = 
-local height = 
+local block = "minecraft:nether_brick_fence"
+local x1 = 21
+local y1 = 80
+local z1 = 498
+local x2 = 21
+local y2 = 81
+local z2 = 505
 
+
+-- in dev
+local openstyle = 0
 
 
 
@@ -23,17 +26,36 @@ if args == nil then
     args = 5
 end
 
-local currentheight = 0
+-- find center
 
-while currentheight < height do
-    exec("fill "..x1.." "..(y+currentheight).." "..z1.." "..x2.." "..(y+currentheight).." "..z2.." minecraft:air")
-    sleep(tonumber(speed))
-    currentheight = (currentheight+1)
+local center = nil
+if x1 == x2 then
+    if z1 < z2 then
+        center = (z1+((z2-z1)/2))
+    elseif z1 > z2 then
+        center = (z2+((z1-z2)/2))
+    end
+elseif z1 == z2 then
+    if x1 < x2 then
+        center = (x1+((x2-x1)/2))
+    elseif x1 > x2 then
+        center = (x2+((x1-x2)/2))
+    end
 end
-sleep(tonumber(args))
-currentheight = (currentheight-1)
-while currentheight >= 0 do
-    exec("fill "..x1.." "..(y+currentheight).." "..z1.." "..x2.." "..(y+currentheight).." "..z2.." "..block)
-    sleep(tonumber(speed))
-    currentheight = (currentheight-1)
-end
+
+print(center)
+
+--local currentheight = 0
+
+--while currentheight < height do
+--    exec("fill "..x1.." "..(y+currentheight).." "..z1.." "..x2.." "..(y+currentheight).." "..z2.." minecraft:air")
+--    sleep(tonumber(speed))
+--    currentheight = (currentheight+1)
+--end
+--sleep(tonumber(args))
+--currentheight = (currentheight-1)
+--while currentheight >= 0 do
+--    exec("fill "..x1.." "..(y+currentheight).." "..z1.." "..x2.." "..(y+currentheight).." "..z2.." "..block)
+--    sleep(tonumber(speed))
+--    currentheight = (currentheight-1)
+--end
