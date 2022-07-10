2fea868321df215ff91997cfca40a0a55320869b
AbsoluteGirth <adrury37@gmail.com>
2022-07-10 02:34:14 +0100

    category selection maybe

--- CashRegister.lua
+++ CashRegister.lua
@@ -8,7 +8,7 @@ local item2price = 2000
 
 local items = {}
 local price = {}
-
+local category = 1
 -- size 51, 19
 
 
@@ -42,7 +42,7 @@ while true do
     term.setCursorPos(1,12)
     term.write("           ")
     term.setCursorPos(1,13)
-    term.write("           ")
+    term.write(" Extras    ")
     term.setCursorPos(1,14)
     term.write("           ")
     term.setCursorPos(1,15)
@@ -56,11 +56,49 @@ while true do
     term.setCursorPos(1,19)
     term.write("           ")
 
+    if category == 1 then
+        term.setBackgroundColor(colors.lightGray)
+        term.setCursorPos(1,5)
+        term.write(" iPiss   > ")
+    elseif category == 2 then
+        term.setBackgroundColor(colors.lightGray)
+        term.setCursorPos(1,7)
+        term.write(" iPoo    > ")
+    elseif category == 3 then
+        term.setBackgroundColor(colors.lightGray)
+        term.setCursorPos(1,9)
+        term.write(" Panels  > ")
+    elseif category == 4 then
+        term.setBackgroundColor(colors.lightGray)
+        term.setCursorPos(1,11)
+        term.write(" Openers > ")
+    elseif category == 5 then
+        term.setBackgroundColor(colors.lightGray)
+        term.setCursorPos(1,13)
+        term.write(" Extras  > ")
+    end
+
     while true do
         local evnet, button, x, y = os.pullEvent("mouse_click")
-        break
-    end
-    break
+        if x <= 11 then
+            if y == 5 then
+                category = 1
+                break
+            elseif y == 7 then
+                category = 2
+                break
+            elseif y == 9 then
+                category = 3
+                break
+            elseif y == 11 then
+                category = 4
+                break
+            elseif y == 13 then
+                category = 5
+                break
+            end
+        end
+    end 
 end
 
 
