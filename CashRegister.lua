eaf49212e68f86b5b86b5afe1c80d5708b7d6381
AbsoluteGirth <adrury37@gmail.com>
2022-07-10 01:52:24 +0100

    Draw sidebar

---
+++ CashRegister.lua
@@ -0,0 +1,28 @@
+-- Cash register --
+
+-- price list
+local item1 = "iPiss"
+local item1price = 50000
+local item2 = "iPiss Case"
+local item2price = 2000
+
+local items = {}
+local price = {}
+
+-- size 51, 19
+
+term.setBackgroundColor(colors.white)
+term.clear()
+term.setBackgroundColor(colors.lightGray)
+term.setCursorPos(1,1)
+term.write("         ")
+term.setCursorPos(1,2)
+term.write(" iPiss   ")
+term.setCursorPos(1,3)
+term.write(" iPoo    ")
+term.setCursorPos(1,4)
+term.write(" Panels  ")
+term.setCursorPos(1,5)
+term.write(" Openers ")
+term.setCursorPos(1,6)
+term.write("         ")
