7f61f7a140b4921e8c53a71e38479bf5c3edaf5e
AbsoluteGirth <adrury37@gmail.com>
2022-07-10 23:45:03 +0100

    Tert functions

--- Store/Delivery/turtle.lua
+++ Store/Delivery/turtle.lua
@@ -9,27 +9,43 @@ local targetz = 0
 local distx = 0
 local disty = 0
 local distz = 0
+local facing = 1
 
 --Leave base
-
-amount = 5
-for i = 1, amount do 
-    turtle.forward()
-end
-turtle.turnRight()
-modem.transmit(30030,1,"open")
-sleep(2)
-amount = 7
-for i = 1, amount do 
-    turtle.forward()
-end
-modem.transmit(30030,1,"close")
-turtle.turnLeft()
-amount = 22
-for i = 1, amount do 
-    turtle.forward()
-end
-amount = 4
-for i = 1, amount do 
+function LeaveBase()
+    amount = 5
+    for i = 1, amount do 
+        turtle.forward()
+    end
+    turtle.turnRight()
+    modem.transmit(30030,1,"open")
+    sleep(2)
+    amount = 7
+    for i = 1, amount do 
+        turtle.forward()
+    end
+    modem.transmit(30030,1,"close")
     turtle.turnLeft()
+    amount = 22
+    for i = 1, amount do 
+        turtle.forward()
+    end
+    amount = 4
+    for i = 1, amount do 
+        turtle.turnLeft()
+    end
 end
+
+function TurnTo(dir)
+    while facing ~= dir do
+        print(facing)
+        turtle.turnRight()
+        facing = facing + 1
+        if facing = 5 then 
+            facing = 1
+        end
+    end
+end
+
+TurnTo(3)
+TurnTo(2)
