01d5af8be04f4a558f76025891674b0b54010adc
AbsoluteGirth <adrury37@gmail.com>
2022-07-10 04:37:22 +0100

    maybe?????

--- CashRegister.lua
+++ CashRegister.lua
@@ -104,15 +104,15 @@ while true do
         -- write values on screen
         term.setTextColor(colors.red)
         local ypos = 2
-        if list1[1] ~= nil then
-            for i in#list do
-                term.setCursorPos(29,i)
-                term.write(list[i])
-                ypos = ypos + 1
-                term.setCursorPos(29,i)
-                term.write(prices[i])
-                ypos = ypos + 1
-            end
+        --if list1[1] ~= nil then
+        for i in#list do
+            term.setCursorPos(29,i)
+            term.write(list[i])
+            ypos = ypos + 1
+            term.setCursorPos(29,i)
+            term.write(prices[i])
+            ypos = ypos + 1
+        
         end
         term.setCursorPos(43,18)
         term.write(total)
