00d3a95f7cbbd282933c6148412987e86f64952f
AbsoluteGirth <adrury37@gmail.com>
2022-07-11 12:01:11 +0100

    ???

--- Store/Delivery/turtle.lua
+++ Store/Delivery/turtle.lua
@@ -143,6 +143,7 @@ while true do
     while true do
         local event,side,chan,rchan,control,dist = os.pullEvent("modem_message")
         print(control)
+        print(target)
         if #target == 2 then 
             if control == "go" then
                 --deliver
