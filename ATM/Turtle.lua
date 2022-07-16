9b65d6f0f62ef7e8392c1490edde4b377c965130
AbsoluteGirth <adrury37@gmail.com>
2022-07-16 09:34:19 +0100

    fgbgbgfd

--- ATM/Turtle.lua
+++ ATM/Turtle.lua
@@ -1,12 +1,26 @@
+local modem = peripheral.find("modem")
+listenPort = 65534
+
+modem.open(listenPort)
+
 function card(control)
     turtle.select(1)
     if control == "take" then
+        turtle.select(1)
         if turtle.suck() == true then
             return("completed")
+        else
+            return("errored")
+        end
     elseif control == "return" then
         if turtle.drop() == true then
+            return("completed")
+        else 
             return("errored")
         end
     end
 end
 
+while true do 
+    local event,side,chan,rchan,control,dist = os.pullEvent("modem_message")
+    print(chan.." "..rchan.." "..control.." "..dist)
