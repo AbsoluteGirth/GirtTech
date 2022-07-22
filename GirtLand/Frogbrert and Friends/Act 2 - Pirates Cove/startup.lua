2c60f1b491747c3ef63a9eacac15051f55c7dda5
AbsoluteGirth <adrury37@gmail.com>
2022-07-22 16:06:28 +0100

    Ok brain cells maybe?

--- GirtLand/Frogbrert and Friends/Act 2 - Pirates Cove/startup.lua
+++ GirtLand/Frogbrert and Friends/Act 2 - Pirates Cove/startup.lua
@@ -11,13 +11,15 @@ end
 
 while true do 
     term.clear()
+    term.setCursorPos(1,1)
     print("Ready")
     local event = os.pullEvent()
     linepos = 1
     term.clear()
+    term.setCursorPos(1,1)
     print("Scene Playing")
     while true do 
-        print("Line "..linepos/5)
+        print("Line "..5/linepos)
         local speaker = scriptlines[linepos]
         local line = scriptlines[linepos+1]
         local linecolor = scriptlines[linepos+2]
@@ -30,7 +32,7 @@ while true do
             break
         elseif speaker == "command" then 
             exec(line)
-            sleep(linecolor)
+            sleep(tostring(linecolor))
         else
             exec("/tellraw @a[r="..radius.."] [{\"text\":\""..speaker.." "..line.."\",\"color\":\""..linecolor.."\",\"bold\":\""..linebold.."\",\"italic\":\""..lineitalic.."\"}]</color></insert>")
             sleep(sleepTime)
