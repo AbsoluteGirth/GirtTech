e797949e8a3b1f39ee39640058db45a8548b3e94
AbsoluteGirth <adrury37@gmail.com>
2022-07-22 15:59:05 +0100

    Final?

--- GirtLand/Frogbrert and Friends/Act 2 - Pirates Cove/startup.lua
+++ GirtLand/Frogbrert and Friends/Act 2 - Pirates Cove/startup.lua
@@ -4,16 +4,20 @@ local radius = 15
 local scriptlines = {}
 local linepos = 1
 
-print("finalnow")
 script = fs.open("/script", "r")
 for line in script.readLine do
     table.insert(scriptlines,line)
 end
 
 while true do 
+    term.clear()
+    print("Ready")
     local event = os.pullEvent()
     linepos = 1
+    term.clear()
+    print("Scene Playing")
     while true do 
+        print("Line "..linepos/5)
         local speaker = scriptlines[linepos]
         local line = scriptlines[linepos+1]
         local linecolor = scriptlines[linepos+2]
@@ -26,6 +30,7 @@ while true do
             break
         elseif speaker == "command" then 
             exec(line)
+            sleep(linecolor)
         else
             exec("/tellraw @a[r="..radius.."] [{\"text\":\""..speaker.." "..line.."\",\"color\":\""..linecolor.."\",\"bold\":\""..linebold.."\",\"italic\":\""..lineitalic.."\"}]</color></insert>")
             sleep(sleepTime)
