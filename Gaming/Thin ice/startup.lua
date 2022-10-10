4b7f737cef7034a9d89977cd9322f3a86627b4eb
AbsoluteGirth <adrury37@gmail.com>
2022-10-10 04:05:29 +0100

    dsas

--- Gaming/Thin ice/startup.lua
+++ Gaming/Thin ice/startup.lua
@@ -1,3 +1,6 @@
+-- wget run https://raw.githubusercontent.com/AbsoluteGirth/GirtTech/main/Gaming/Thin%20ice/startup.lua
+
 mon = peripheral.find("monitor")
 
+mon.setTextScale(1)
 mon.write("balls")
