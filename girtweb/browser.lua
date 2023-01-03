cddcbba2982fc37864f034263cbde1fc3da4d3a8
AbsoluteGirth <adrury37@gmail.com>
2023-01-03 00:14:10 +0000

    aaa

---
+++ girtweb/browser.lua
@@ -0,0 +1,11 @@
+function drawButton(xPos, yPos, text, bgColor, textColor, redirect)
+
+end
+
+function drawNavBar()
+    term.setCursorPos(1,1)
+    term.setBackgroundColor(colors.gray)
+    term.getSize()
+    term.write()
+
+end
