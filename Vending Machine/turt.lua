2cbe7d3967f3f220be0152d4575f81187c80790f
AbsoluteGirth <adrury37@gmail.com>
2023-05-19 19:39:10 +0100

    Forgot the thens

--- Vending Machine/turt.lua
+++ Vending Machine/turt.lua
@@ -7,15 +7,15 @@ while true do
     if dist < 2 then 
         if message = "a0" then 
             turtle.select(5)
-        elseif message = "a1"
+        elseif message = "a1" then
             turtle.select(6)
-        elseif message = "a2"
+        elseif message = "a2" then
             turtle.select(7)
-        elseif message = "c0"
+        elseif message = "c0" then
             turtle.select(13)
-        elseif message = "c1"
+        elseif message = "c1" then
             turtle.select(14)
-        elseif message = "c2"
+        elseif message = "c2" then
             turtle.select(15)
     end
 
