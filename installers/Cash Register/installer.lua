348a90e1ad1b867fb19eb246362eea0518a84ff7
AbsoluteGirth <adrury37@gmail.com>
2023-06-23 20:21:35 +0100

    a

--- installers/Cash Register/installer.lua
+++ installers/Cash Register/installer.lua
@@ -18,41 +18,64 @@ config = {
 
 function install(i)
     local tempFile = http.get(files[i].url)
+    if not tempFile then 
+        error("Invalid URL for file: "..files[i].file)
+    end
+    print("Installing "..files[i].file)
+    drawProgBar()
     f = fs.open(files[i].file, "w")
-    f.write(tempFile)
+    f.write(tempFile.readAll())
     f.close()
 end 
 
-function getFiles()
-    for i=1, #files do
-        install(i)
-    end
-end
-
 function checkUpdate()
     for i=1, #files do 
-        if files[i].update then 
+        if files[i].update == true then 
             local newestVer = http.get(files[i].url)
             f = fs.open(files[i].file, "r")
             local installedVer = f.readAll()
             f.close()
 
-            if newestVer ~= installedVer then 
+            if newestVer.readAll() ~= installedVer then 
+                print("Installing update for "..files[i].file)
                 install(i)
             end 
         end
     end
 end
 
+function drawProgBar()
+    print("<------------>")
+    local x, y = term.getCursorPos()
+    term.setCursorPos(2, y-1)
+    for i=1,12 do 
+        term.write("*")
+        sleep(math.random(1, 8)/10)
+    end
+    term.setCursorPos(x, y)
+    
+end
+
 function checkInstalled()
     for i=1, #files do 
-        if not fs.exists(reqFiles[i]) then 
+        if not fs.exists(files[i].file) then 
             install(i)
         end
     end
 end 
 
+function checkFileIntegrity()
+    for i=1, #config.reqFiles do 
+        if not fs.exists(config.reqFiles[i]) then 
+            error("File "..config.reqFiles[i].." is missing, try reinstalling the program.")
+        end
+    end
+end
+
 function run()
+    if not fs.exists(config.programFile) then 
+        error("Program file not found")
+    end
     shell.run(config.programFile)
 end
 
@@ -61,7 +84,13 @@ term.setCursorPos(1,1)
 
 print("Starting...")
 checkInstalled()
+sleep(0.5)
 print("Checking for updates...")
 checkUpdate()
+sleep(0.5)
+print("Checking file integrity...")
+checkFileIntegrity()
+
+sleep(0.5)
 
 run()
---
+++ installers/installerinstaller.lua
@@ -0,0 +1,38 @@
+url = "https://raw.githubusercontent.com/AbsoluteGirth/GirtTech/main/installers/Cash%20Register/installer.lua"
+
+
+function drawProgBar()
+    print("<------------>")
+    local x, y = term.getCursorPos()
+    term.setCursorPos(2, y-1)
+    for i=1,12 do 
+        term.write("*")
+        sleep(math.random(1, 8)/10)
+    end
+    term.setCursorPos(x, y)
+    
+end
+
+
+
+local tempFile = http.get(url)
+if not tempFile then 
+    error("Invalid URL")
+end
+print("Installing...")
+drawProgBar()
+f = fs.open(files[i].file, "w")
+f.write(tempFile.readAll())
+f.close()
+sleep(0.5)
+term.clear()
+term.setCursorPos(1,1)
+print("Installation complete!")
+print("Restarting in, ")
+for i=1,6 do 
+    term.setCursorPos(15, 2)
+    term.write(6-i)
+    sleep(1)
+end
+
+os.reboot()
